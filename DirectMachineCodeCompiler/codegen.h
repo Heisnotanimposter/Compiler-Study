b7336e45545ef012ef4396c873e5d2045b05268e
Seungwon Lee <97718938+Heisnotanimposter@users.noreply.github.com>
2026-03-03 10:28:41 +0900

    Update codegen.h

--- DirectMachineCodeCompiler/codegen.h
+++ DirectMachineCodeCompiler/codegen.h
@@ -12,10 +12,21 @@ void emit_add_rax_rbx();
 void emit_sub_rax_rbx();
 void emit_imul_rax_rbx();
 void emit_idiv_rax_rbx();
+void emit_cmp_rax_rbx();
+void emit_set_eq();
+void emit_set_lt();
+void emit_set_gt();
+void emit_test_rax_rax(); // for if(cond)
 void emit_push_rax();
 void emit_pop_rax();
 void emit_pop_rbx();
 void emit_ret();
+
+// Jumps and Patching
+int  emit_jmp_placeholder();  // returns position of displacement to patch
+int  emit_jz_placeholder();   // returns position of displacement to patch
+int  emit_jnz_placeholder();  // returns position of displacement to patch
+void patch_jump_to_current(int patch_pos);
 
 // Variable handling
 void emit_mov_var_rax(int offset); // mov [rbp-offset], rax
